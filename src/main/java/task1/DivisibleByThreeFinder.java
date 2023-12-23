004315af9b731d3c96f72a2e0fcd7fe7387938b8
Damian Temimi <damian.temimi@gmail.com>
2023-12-23 01:28:17 +0100

    changes

--- src/main/java/task1/DivisibleByThreeFinder.java
+++ src/main/java/task1/DivisibleByThreeFinder.java
@@ -5,13 +5,10 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DivisibleByThreeFinder {
-    public static void findAndDisplayNumbersDivisibleByThree(int startRange, int endRange) {
-        List<Integer> numbersDivisibleByThree = IntStream.rangeClosed(startRange, endRange)
+    public static List<Integer> findNumbersDivisibleByThree(int startRange, int endRange) {
+        return IntStream.rangeClosed(startRange, endRange)
                 .filter(num -> num % 3 == 0)
                 .boxed()
                 .collect(Collectors.toList());
-
-        System.out.println("Liczby podzielne przez 3: ");
-        numbersDivisibleByThree.forEach(System.out::println);
     }
 }
--- src/test/java/task1/DivisibleByThreeFinderTest.java
+++ src/test/java/task1/DivisibleByThreeFinderTest.java
@@ -1,28 +1,39 @@
 package task1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DivisibleByThreeFinderTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
-    public void testFindAndDisplayNumbersDivisibleByThree() {
+    public void testFindNumbersDivisibleByThree() {
         // Given
         int startRange = 1;
         int endRange = 20;
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
+        String expectedOutput = "Numbers divisible by 3:\n" +
+                "3\n6\n9\n12\n15\n18\n";
 
         // When
-        DivisibleByThreeFinder.findAndDisplayNumbersDivisibleByThree(startRange, endRange);
-        String expectedOutput = "Liczby podzielne przez 3: \r\n3\r\n6\r\n9\r\n12\r\n15\r\n18\r\n";
+        List<Integer> result = DivisibleByThreeFinder.findNumbersDivisibleByThree(startRange, endRange);
 
         // Then
-        assertEquals(expectedOutput, outContent.toString());
+        StringBuilder outputBuilder = new StringBuilder();
+        outputBuilder.append("Numbers divisible by 3:\n");
+        result.forEach(num -> outputBuilder.append(num).append("\n"));
+        String actualOutput = outputBuilder.toString();
+
+        assertEquals(expectedOutput, actualOutput);
     }
 }
